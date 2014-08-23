ea47bb6aaa8159d114c94d03aa71963839d25e1c
Sreenidhy Sreepathy <sreenidhy@yahoo-inc.com>
2014-08-23 16:14:27 -0700

    Testng support added

---
+++ src/test/java/skeleton/RunCukesTest.java
@@ -0,0 +1,12 @@
+package skeleton;
+
+/**
+ * Created by sreenidhy on 8/23/14.
+ */
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(format = "pretty")
+public class RunCukesTest extends AbstractTestNGCucumberTests {
+}
