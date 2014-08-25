00a8014aa515dcf9c3d72b33735a1ee377871d24
Sreenidhy Sreepathy <sreenidhy@yahoo-inc.com>
2014-08-24 22:14:48 -0700

    Added belly growl and last eat time

--- src/main/java/skeleton/Belly.java
+++ src/main/java/skeleton/Belly.java
@@ -4,7 +4,23 @@ package skeleton;
  * Created by sreenidhy on 8/23/14.
  */
 public class Belly {
+
+    private long lastEatTime;
+
     public void eat(int cukes) {
 
+    }
+
+
+    public long getLastEatTime() {
+        return lastEatTime;
+    }
+
+    public void setLastEatTime(long lastEatTime) {
+        this.lastEatTime = lastEatTime;
+    }
+
+    public void growl() {
+        System.out.println("Growwwwlll!!");
     }
 }
--- src/test/java/skeleton/MyStepdefs.java
+++ src/test/java/skeleton/MyStepdefs.java
@@ -9,24 +9,33 @@ import cucumber.api.java.en.When;
  * Created by sreenidhy on 8/23/14.
  */
 public class MyStepdefs {
+
+    Belly belly = new Belly();
+
     @Given("^I hve (\\d+) cukes in my belly$")
     public void I_hve_cukes_in_my_belly(int arg1) throws Throwable {
         // Express the Regexp above with the code you wish you had
         System.out.println("Create new Belly with " + arg1 + " cukes");
+
+        belly.eat(arg1);
+        belly.setLastEatTime(System.nanoTime());
         //throw new PendingException();
     }
 
     @When("^I wait (\\d+) hour$")
     public void I_wait_hour(int arg1) throws Throwable {
         // Express the Regexp above with the code you wish you had
-        System.out.println("Almost 1 hour!!");
+        long waitTime = System.nanoTime() - belly.getLastEatTime();
+        System.out.println("Time since belly was fed: " + waitTime);
+        System.out.println("Almost an hour now!!");
         /*throw new PendingException();*/
     }
 
     @Then("^my belly should growl$")
     public void my_belly_should_growl() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        System.out.println("Belly Growwwll!!");
+        //System.out.println("Belly Growwwll!!");
+        belly.growl();
         //throw new PendingException();
     }
 }
